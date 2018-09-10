1785e0ae9faf761b1ef8f4fb99a48f0d5db5e292
Ringenberg <Ringenberg@users.noreply.github.com>
2018-09-10 08:51:24 -0400

    Improvements to test to handle the async nature of this program.

--- test/test.js
+++ test/test.js
@@ -1,13 +1,19 @@
 const mic = require('mocha-image-compare')({});
 const assert = require('assert');
 const cytopng = require('../index.js');
+const fs = require('fs');
 
 describe('cytoscape2png', function() {
   this.timeout(0);
-  before('Generate Graph1.png', function() {
-    return cytopng('test/Graph1.json');
+  before('Generate Graph1.png', async function() {
+    if (fs.existsSync('test/Graph1.png')) {
+      fs.unlinkSync('test/Graph1.png')
+    }
+    var cpg = await cytopng('test/Graph1.json');
+    cpg;
+    return
   });
-  it('Graph1', function(done) {
+  it('Graph1', /*async*/ function(done) {
     let compare = mic.test(this);
     compare('test/Graph1.png', 0.001, 'test/Graph1-result.png', done);
   });
