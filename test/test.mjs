d496ac9b48f0aae527d7a073bd181371053aa699
Michael Ringenberg <mringenb@andrew.cmu.edu>
2022-10-21 14:25:30 -0400

    Updates dependencies.  Updates test to use async looksSame.

--- test/test.mjs
+++ test/test.mjs
@@ -3,21 +3,22 @@ import test from 'ava';
 import cytopng from '../index.js';
 import looksSame from 'looks-same';
 
-test.before('Generate Graph1', async t => {
+test.before('Generate Graph1', async (t) => {
   if (fs.existsSync('test/Graph1.png')) {
-    fs.unlinkSync('test/Graph1.png')
+    fs.unlinkSync('test/Graph1.png');
   }
   await cytopng(['test/Graph1.json']);
 });
-test.after('Cleanup product', t => {
+test.after('Cleanup product', (t) => {
   if (fs.existsSync('test/Graph1.png')) {
-    fs.unlinkSync('test/Graph1.png')
+    fs.unlinkSync('test/Graph1.png');
   }
 });
 
-test('cytoscape2png', t => {
-  return new Promise((resolve, reject) =>
-    looksSame('test/Graph1.png', 'test/Graph1-result.png', {ignoreAntialiasing: true, antialiasingTolerance: 3}, (error, {equal}) => {
-      error ? reject(error) : resolve(equal)
-    })).then(t.truthy)
+test('cytoscape2png', async (t) => {
+  const res = await looksSame('test/Graph1.png', 'test/Graph1-result.png', {
+    ignoreAntialiasing: true,
+    antialiasingTolerance: 3,
+  });
+  t.truthy(res.equal);
 });
