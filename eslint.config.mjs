706ca6b06a96ab901be343a94ba971090936e5a4
Michael Ringenberg <Ringenberg@users.noreply.github.com>
2026-02-27 14:28:39 +0000

    Bump eslint to 10.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import { defineConfig } from "eslint/config";
 
-
-export default [
-  {languageOptions: { globals: globals.node }},
-  pluginJs.configs.recommended,
-];
+export default defineConfig([
+  { languageOptions: { globals: globals.node } },
+  {
+    files: ["index.js", "**/*.m?js"],
+    plugins: { pluginJs }
+  }
+]);
